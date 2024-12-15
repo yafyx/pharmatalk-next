5609d5e2bee7558d1511acada7cb1415d4ee2a15
fyx <info@yfyx.me>
2024-12-15 12:53:29 +0700

    Fix ChatInput and ChatMessageList components

--- components/ui/chat/chat-input.tsx
+++ components/ui/chat/chat-input.tsx
@@ -2,22 +2,21 @@ import * as React from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
 
-interface ChatInputProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement>{}
-
-const ChatInput = React.forwardRef<HTMLTextAreaElement, ChatInputProps>(
-  ({ className, ...props }, ref) => (
-    <Textarea
-      autoComplete="off"
-      ref={ref}
-      name="message"
-      className={cn(
-        "max-h-12 px-4 py-3 bg-background text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 w-full rounded-md flex items-center h-16 resize-none",
-        className,
-      )}
-      {...props}
-    />
-  ),
-);
+const ChatInput = React.forwardRef<
+  HTMLTextAreaElement,
+  React.TextareaHTMLAttributes<HTMLTextAreaElement>
+>(({ className, ...props }, ref) => (
+  <Textarea
+    autoComplete="off"
+    ref={ref}
+    name="message"
+    className={cn(
+      "max-h-12 px-4 py-3 bg-background text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 w-full rounded-md flex items-center h-16 resize-none",
+      className
+    )}
+    {...props}
+  />
+));
 ChatInput.displayName = "ChatInput";
 
 export { ChatInput };
