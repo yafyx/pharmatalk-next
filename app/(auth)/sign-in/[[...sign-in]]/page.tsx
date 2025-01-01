81ff6e72f8c1e9a4cd2810bba0ca294d68ef64f8
fyx <info@yfyx.me>
2025-01-01 14:25:15 +0700

    Update auth pages

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,8 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <main className="min-h-screen w-full flex items-center justify-center p-4 bg-gray-50">
-      <div className="w-full max-w-md">
+    <main className="w-full flex items-center justify-center p-4 bg-white min-h-screen">
+      <div className="w-full max-w-md z-10">
         <SignIn
           appearance={{
             elements: {
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,8 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <main className="min-h-screen w-full flex items-center justify-center p-4 bg-gray-50">
-      <div className="w-full max-w-md">
+    <main className="min-h-screen w-full flex items-center justify-center p-4 bg-white">
+      <div className="w-full max-w-md z-10">
         <SignUp
           appearance={{
             elements: {
